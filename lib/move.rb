829727b5f0697b3547ce14467c30496e14b6a109
Mike Doig <mike.j.doig@gmail.com>
2019-02-20 18:40:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+# def input_to_index(user_input)
+#   board_index = parseInt(${user_input}) - 1
+#end
+
+#def move
+  
+#end
