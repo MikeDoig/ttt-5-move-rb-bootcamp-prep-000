2e52f10ff465e3b8b1d3ca0aa66042e93b3b677f
Mike Doig <mike.j.doig@gmail.com>
2019-02-20 18:45:56 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,9 +7,11 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input.to_i
+  user_input = user_input.to_i-1
 end
 
-def move
-  
+def move(board, index, character = "X")
+
+
+puts display_board  
 end
